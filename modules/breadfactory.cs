49b6b8b67afe09da3096199eb55d16f48927fd75
Emerald-AM9 <emeraldam9@ms-rss.com>
2022-12-02 10:37:41 +0800

    Mica App 5.1.50

--- modules/breadfactory.cs
+++ modules/breadfactory.cs
@@ -1,12 +1,4 @@
-﻿//Mica App提供一个2KBOT的稳定存档，一般情况下请使用一个中版本下最新的小版本。
-//Copyright(C) 2022 Emerald-AM9 版权所有。
-
-//本程序是自由软件：你可以根据自由软件基金会发布的GNU Affero通用公共许可证的条款，即许可证的第3版或（您选择的）任何后来的版本重新发布它和/或修改它。。
-
-//本程序的发布是希望它能起到作用。但没有任何保证；甚至没有隐含的保证。本程序的分发是希望它是有用的，但没有任何保证，甚至没有隐含的适销对路或适合某一特定目的的保证。 参见 GNU Affero通用公共许可证了解更多细节。
-
-//您应该已经收到了一份GNU Affero通用公共许可证的副本。 如果没有，请参见<https://www.gnu.org/licenses/>。
-using MySql.Data.MySqlClient;
+﻿using MySql.Data.MySqlClient;
 using System.Data;
 
 namespace Net_2kBot.Modules
